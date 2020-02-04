cd5df74828114180d5ef20da3e2c7b4eb14c38da
Dave Letorey <dletorey@squiz.co.uk>
2020-02-04 17:30:39 +0000

    updated to only read one main element

--- JS/calculateReadingTime.js
+++ JS/calculateReadingTime.js
@@ -1,6 +1,6 @@
 window.calcRT = ev => {
-    const content = document.querySelectorAll('main'); // grabs all the content in the <main> element
-    const numWords = content[0].innerText.split(' ').length; //counts number of words in the content
+    const content = document.querySelector('main'); // grabs all the content in the <main> element
+    const numWords = content.innerText.split(' ').length; //counts number of words in the content
     const wordsPerMinute = 200; // Average reading speed.
     let result; // sets up a result to inject into the page
     if(numWords > 0){ // checks to see that there is actuallt some content
